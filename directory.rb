364089ccf30a55228b1b4f405ee7fe956b8fdbbd
Ruth Baker <ruthmoog@gmail.com>
2019-05-16 15:31:13 +0100

    Print only students with names beginning with B

--- directory.rb
+++ directory.rb
@@ -32,9 +32,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index| 
-    puts "#{index + 1}. #{student[:name]} \
-    (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    if student[:name][0] == "B"
+      puts "#{index + 1}. #{student[:name]} \
+      (#{student[:cohort]} cohort)"
+    end
   end
 end
 
