3ca28c90a882dce537e3bffc5de2fe3810f892df
Ruth Baker <ruthmoog@gmail.com>
2019-05-16 14:52:53 +0100

    Add cohort data to students array and store data in hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # store student names in array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Kruger",
-"The Joker",
-"Joffrey Barathron",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Kruger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Barathron", cohort: :november},
+{name: "Norman Bates",cohort: :november},
 ]
 
 def print_header
@@ -18,12 +18,12 @@ def print_header
   puts "-----"
 end
 
-def print(names)
-  names.each { |student| puts student }
+def print(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
 end
 
 print_header
