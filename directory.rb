05671de865ee1caad38c714e6564de2c94280872
Ruth Baker <ruthmoog@gmail.com>
2019-05-16 14:37:38 +0100

    Refactor students into array, automate count, mvoe data from operation

--- directory.rb
+++ directory.rb
@@ -1,19 +1,23 @@
-student_count = 11
+# store student names in array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Kruger",
+"The Joker",
+"Joffrey Barathron",
+"Norman Bates"
+]
 
 #print the list of students
 puts "The students of Villains Academy"
 puts "-----"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
-puts "Joffrey Barathron"
-puts "Norman Bates"
+students.each { |student| puts student }
+
 
 #print the total number of students
-puts "\nOverall, we have #{student_count} great students."
+puts "\nOverall, we have #{students.count} great students."
