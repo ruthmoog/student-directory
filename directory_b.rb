e3c3b8c9308ac3c1de8840d9c11fa626ec93edd1
Ruth Baker <ruthmoog@gmail.com>
2019-05-16 17:09:31 +0100

    Parse cohort data only accepting active cohorts in specific format

--- directory_b.rb
+++ directory_b.rb
@@ -6,20 +6,34 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     student_info = get_more_data
-    students << {name: name, cohort: :november, nemisis: student_info[0], color: student_info[1]}
+    students << {name: name, cohort: student_info[0], nemisis: student_info[1], color: student_info[2]}
     puts "Now we have #{students.count} students"
     puts "Name:"
     name = gets.chomp
   end
+  puts student_info
   students
 end
 
+COHORTS = ["March", "April", "June", "November"]
+
 def get_more_data
+  puts "Cohort:"
+  cohort = gets.chomp
+  if cohort == ""
+    cohort = Time.now.strftime("%B")
+  end
+  until COHORTS.include? cohort
+    puts "Cohort '#{cohort}' not valid. Make sure to enter full month name eg 'June':"
+    puts "Active cohorts: #{COHORTS.join(", ")}"
+    cohort = gets.chomp
+  end
+  cohort.capitalize.to_sym
   puts "Nemisis:"
-  nemisis = gets.chomp
+  nemisis = gets.chomp.downcase.to_sym
   puts "Favourite colour:"
-  color = gets.chomp
-  [nemisis, color]
+  color = gets.chomp.downcase.to_sym
+  [cohort, nemisis, color]
 end
 
 def print_header
