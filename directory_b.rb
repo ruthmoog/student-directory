f895ce96b14909af98aa1faa378e04e918a3d119
Ruth Baker <ruthmoog@gmail.com>
2019-05-21 12:31:24 +0100

    Add read (load) and write (save) file interaction

--- directory_b.rb
+++ directory_b.rb
@@ -69,9 +69,10 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
-
 
 def show_students
   if @students.count > 0
@@ -82,12 +83,35 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
